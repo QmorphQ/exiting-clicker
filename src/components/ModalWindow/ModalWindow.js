714651d27c6cb0bd9efbf1d2e634e7fb1ac49086
Владислав Маковский <alectorkinga@gmail.com>
2023-04-14 07:54:22 +0300

    post cleaning

--- src/components/ModalWindow/ModalWindow.js
+++
@@ -1,6 +0,0 @@
-import sheet from './ModalWindow.css' assert { type: 'css' };
-document.adoptedStyleSheets = [sheet];
-const modalWindow = document.createElement("div");
-modalWindow.setAttribute("class", "modal-window");
-modalWindow.innerHTML = "<h1>Hello</h1>";
-export default modalWindow;
